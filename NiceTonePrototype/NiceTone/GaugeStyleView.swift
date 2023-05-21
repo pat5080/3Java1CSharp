f8d602187cd11aef40f967e7cb1084192597c70c
Patrick Korczak <36663050+pat5080@users.noreply.github.com>
2023-05-21 19:26:39 +1000

    Added more dynamic functionality. Improved UI.

--- NiceTonePrototype/NiceTone/GaugeStyleView.swift
+++ NiceTonePrototype/NiceTone/GaugeStyleView.swift
@@ -9,10 +9,46 @@ import Foundation
 import SwiftUI
 
 struct GaugeStyleView: GaugeStyle{
-    private var gradient = AngularGradient(gradient: Gradient(colors: [.red, .green, .red]), center: .center, startAngle: .degrees(180), endAngle: .degrees(360))
+    public var gradient = AngularGradient(gradient: Gradient(colors: [.red, .green, .red]), center: .center, startAngle: .degrees(180), endAngle: .degrees(360))
+    
+    let notes = ["C","C#","D","D#","E","F","F#","G","G#","A","A#","B"]
+    let freq = [261.0, 277.0, 293.0, 311.0, 329.0, 349.0, 370.0, 392.0, 415.0, 440.0, 466.0, 493.0]
+    
+    @Binding var targetFrequency: Double
+    @Binding var targetNote: String
     
     func makeBody(configuration: Configuration) -> some View {
-        VStack {
+            VStack(alignment: .customCenter) {
+                Text("Target")
+                    .font(.system(size: 30)).bold()
+                HStack {
+                    Button(action: {
+                        targetFrequency = setCurrentFrequency(note: targetNote)
+                    }) {
+                        Image(systemName: "arrow.counterclockwise")
+                    }
+                    Button(action: {
+                        if targetFrequency > setCurrentFrequency(note: targetNote) {
+                            targetFrequency -= 1
+                        }
+                    }) {
+                        Image(systemName: "minus.circle")
+                    }
+                    
+                    Text("\(String(format: "%.2f", targetFrequency))")
+                        .font(.system(size: 30))
+                        .alignmentGuide(.customCenter) {
+                            $0[HorizontalAlignment.center]
+                        }
+                    Text("Hz")
+                        .font(.title3)
+                    
+                    Button(action: {
+                        targetFrequency += 1
+                    }) {
+                        Image(systemName: "plus.circle")
+                    }
+                }
             ZStack() {
                 configuration.label
                 Arc()
@@ -26,7 +62,7 @@ struct GaugeStyleView: GaugeStyle{
                     .stroke(Color.black, style: StrokeStyle(lineWidth: 7, lineCap: .butt, lineJoin: .round, dash: [3, 36], dashPhase: 0.0))
                     //.rotationEffect(.degrees(180))
             }
-            .frame(height: 100)
+            .frame(height: 150)
             HStack {
                 configuration.minimumValueLabel
                 Spacer()
@@ -35,9 +71,10 @@ struct GaugeStyleView: GaugeStyle{
                 configuration.maximumValueLabel
             }
         }
-        .frame(width: 300, height: 300)
+        .frame(width: 300, height: 280)
     }
 }
+
 
 struct Arc: Shape {
     func path(in rect: CGRect) -> Path {
